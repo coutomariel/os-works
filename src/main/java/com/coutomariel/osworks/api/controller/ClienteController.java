c0e448ecbe69ecc82d85b093f73766cd4af4f0b1
Mariel <coutomariel@gmail.com>
2020-08-19 23:09:00 -0300

    Criadas operações de CRUD da entidade cliente

--- src/main/java/com/coutomariel/osworks/api/controller/ClienteController.java
+++ src/main/java/com/coutomariel/osworks/api/controller/ClienteController.java
@@ -1,30 +1,67 @@
 package com.coutomariel.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coutomariel.osworks.domain.model.Cliente;
+import com.coutomariel.osworks.domain.repository.ClientesRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
-	@GetMapping("/clientes")
+	@Autowired
+	ClientesRepository clienteRepository;
+	
+	@GetMapping
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João das Neves");
-		cliente1.setEmail("jneves@teste.com");
-		cliente1.setTelefone("33221418");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Brandon Stark");
-		cliente2.setEmail("brandon@teste.com");
-		cliente2.setTelefone("33181732");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll(); 
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscarCliente(@PathVariable Long id) {
+		Optional<Cliente> response = clienteRepository.findById(id);
+		if(response.isPresent()) {
+			return ResponseEntity.ok(response.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Cliente salvarCliente(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+		if(!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(id);
+		Cliente response = clienteRepository.save(cliente);
+		return ResponseEntity.ok(response);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
+		if(!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
---
+++ src/main/java/com/coutomariel/osworks/domain/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package com.coutomariel.osworks.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.coutomariel.osworks.domain.model.Cliente;
+
+@Repository
+public interface ClientesRepository extends JpaRepository<Cliente, Long> {
+
+}
